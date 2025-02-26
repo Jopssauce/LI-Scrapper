7a02db4a786eda9f9b43f7a89f551e43d2cec43c
unknown <johnasuncion0304@gmail.com>
2025-02-25 18:19:49 -0800

    Souped jobs

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pprint
 
 
 li_url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?_l=en_US&keywords=Software%20Engineer&location=San%20Francisco%20Bay%20Area&geoId=90000084&f_TPR=r86400&start={}"
-target_url='https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Python%20%28Programming%20Language%29&location=Las%20Vegas%2C%20Nevada%2C%20United%20States&geoId=100293800&currentJobId=3415227738&start={}'
 test_url = "http://lumtest.com/myip.json"
 success = False
 
@@ -21,17 +20,18 @@ rand_proxy = random.choice(proxies_list)
 # BrightData Proxies not working for linkedin. Maybe because of trial account.
 # Okay using only an HTTP proxy works but is NOT SAFE
 # https://stackoverflow.com/questions/69220126/getting-ssl-certificate-verify-failed-when-using-proxy-with-python-requests
-resp = requests.get(li_url.format(25), proxies=config['proxies'], headers=config['headers'])
+resp = requests.get(li_url.format(25), proxies=config['proxies'], headers=config['headers'], stream=True)
 
 if(resp.status_code != 200):
-    print(f"Failed with {resp.status_code} {rand_proxy}")
+    print(f"Failed with {resp.status_code}")
 else:
-    print(f"Success - {rand_proxy} - {resp.status_code}")
+    print(f"Success - {resp.status_code}")
     success = True
     #break
 
 if success:
-    soup = BeautifulSoup(resp.raw, 'html.parser')
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    jobs = soup.find_all("h3")
     #print(soup.find('h4', class_='base-search-card__title'))
     #pprint.pprint(resp.text)
-    print(resp.text)
+    print(jobs)
