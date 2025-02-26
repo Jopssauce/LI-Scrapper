cb65bc28a5cba3f1d5031371e7f158c7edaac11e
unknown <johnasuncion0304@gmail.com>
2025-02-26 13:16:10 -0800

    Extracted description and technologies used

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ success = False
 
 config = json.load(open("scrapper-config.json"))
 proxies_list = config['proxies_list']
-#for _ in proxies_list: 
 rand_proxy = random.choice(proxies_list)
+
+languages = (config["Languages"])
+techs = (config["Techs"])
 
 # Free proxies is failing because I need SSL Verification
 # Or the page is being populated by JavaScript?
 # BrightData Proxies not working for linkedin. Maybe because of trial account.
 # Okay using only an HTTP proxy works but is NOT SAFE
 # https://stackoverflow.com/questions/69220126/getting-ssl-certificate-verify-failed-when-using-proxy-with-python-requests
-resp = requests.get(li_url.format(25), proxies=config['proxies'], headers=config['headers'], stream=True)
+resp = requests.get(li_url.format(25), proxies=config['proxies'], headers=config['headers'])
 
 if(resp.status_code != 200):
     print(f"Failed with {resp.status_code}")
@@ -31,7 +33,26 @@ else:
 
 if success:
     soup = BeautifulSoup(resp.text, 'html.parser')
-    jobs = soup.find_all("h3")
+    jobs = soup.find_all("a", class_ = 'base-card__full-link', href=True)
     #print(soup.find('h4', class_='base-search-card__title'))
     #pprint.pprint(resp.text)
-    print(jobs)
+    
+    #for j in jobs:
+    #    print(j['href'])
+
+target_resp = requests.get(jobs[0]['href'], proxies=config['proxies'], headers=config['headers'])
+
+if(target_resp.status_code == 200):
+    target_soup = BeautifulSoup(target_resp.text, 'html.parser')
+    target_job = target_soup.find('div', class_ = 'show-more-less-html__markup')
+    print(target_job.contents)
+
+for i in languages:
+    res = target_job.text.find(i)
+    if (res != -1):
+        print(i)
+
+for i in techs:
+    res = target_job.text.find(i)
+    if (res != -1):
+        print(i)
